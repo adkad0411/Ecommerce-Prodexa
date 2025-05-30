61c264b8b4018e7d0662d6ad976dc0798ce424be
adam <adamkadri0406pgmail.com>
2025-05-31 00:38:00 +0100

    make Dynamic search Bar

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title'
 import ProductItem from '../components/ProductItem'
 import { assets } from '../assets/frontend_assets/assets'
 const Collection = () => {
-  const {products}=useContext(ShopContext)
+  const {products,search,showSearch}=useContext(ShopContext)
   const [showFilter,setShowFilter]=useState(false);
   const [filterProducts,setFilterProducts]=useState([]);
   const [category,setCategory]=useState([]);
@@ -31,6 +31,9 @@ const Collection = () => {
 
   const applyFilter=()=>{
     let productsCopy=products.slice();
+    if(showSearch&&search){
+      productsCopy=productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()));
+    }
     if(category.length >0){
       productsCopy=products.filter(item=>category.includes(item.category));
     }
@@ -55,7 +58,7 @@ const Collection = () => {
   }
   useEffect(()=>{
     applyFilter()
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
   useEffect(()=>{
     sortProduct()
   },[sortType])
