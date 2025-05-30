ff6f45329513f8d23ad8965f778078d0482b070b
adam <adamkadri0406pgmail.com>
2025-05-30 11:57:23 +0100

    setup home page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+    <div>
+        <img src={assets.logo} className='mb-5 w-32' alt="" />
+        <p className='w-full md:w-2/3 text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci optio ipsum ipsa pariatur accusamus quo quia possimus, repellendus cupiditate, velit mollitia consequuntur numquam, ad quos quae illo totam voluptate inventore voluptas minima! Quasi repudiandae vero rem voluptates reiciendis, sapiente totam! Ipsam possimus, quisquam suscipit accusantium optio esse laudantium necessitatibus quas.</p>
+    </div>
+    <div>
+        <p className='text-xl font-medium mb-5'>COMPANY</p>
+        <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+        </ul>
+    </div>
+
+    <div>
+        <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+        <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>+1-213-456-7890</li>
+            <li>prodexa.company@gmail.com</li>
+        </ul>
+    </div>
+      </div>
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center '>
+            © 2025 Prodexa. All rights reserved.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
+import BestSeller from '../components/BestSeller'
+import LatestCollection from '../components/LatestCollection'
+import OurPolicy from '../components/OurPolicy'
+import NewsLetter from '../components/NewsLetter'
 const Home = () => {
   return (
     <div>
-      
+      <Hero/>
+      <LatestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsLetter/>
     </div>
   )
 }
